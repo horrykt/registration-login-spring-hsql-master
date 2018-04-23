ce5a8b3923ebefaf2d26186118d906bf00afc75d
Horia Trandafir <horia.trandafir@gmail.com>
2018-04-23 11:25:42 +0300

    first commit

---
+++ src/main/java/com/hellokoding/auth/model/User.java
@@ -0,0 +1,70 @@
+package com.hellokoding.auth.model;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+
+
+
+    private String userEmail;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return userEmail;
+    }
+
+    public void setUsername(String username) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Transient
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+    @ManyToMany
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
